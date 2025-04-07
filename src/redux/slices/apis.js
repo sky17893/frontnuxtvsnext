c8b54d23be0a85fd36665bde0445c948a7930766
sky17893 <sky881012@naver.com>
2025-04-07 17:12:12 +0900

    Update apis.js

--- src/redux/slices/apis.js
+++ src/redux/slices/apis.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "https://lawmangback.aicc4hyeonji.site";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://lawmangback.aicc4hyeonji.site";
+//export const BASE_URL = "http://localhost:8000";
